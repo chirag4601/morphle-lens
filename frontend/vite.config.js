7bf49f2f2c0c5c25aae7624e56feb42435e66322
chirag4601 <chirag4601@gmail.com>
2025-01-20 23:09:43 +0530

    [Fix]: Build fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  publicDir: "./public",
+  base: "",
+  build: {
+    manifest: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) =>
+          assetInfo.name?.endsWith(".css")
+            ? "assets/index.min.css"
+            : "[name][extname]",
+        entryFileNames: () => "assets/index.min.js",
+      },
+    },
+  },
+});
