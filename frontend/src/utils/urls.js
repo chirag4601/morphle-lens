f8a2740463e156478c1a4298a8458627327482d6
chirag4601 <chirag4601@gmail.com>
2025-01-21 23:21:29 +0530

    Added env file in frontend for base url

---
+++ frontend/src/utils/urls.js
@@ -0,0 +1,4 @@
+export const GET_STATE_URL = "/backend/api/state/";
+export const MOVE_MACHINE_URL = "/backend/api/move/";
+export const FOCUS_MACHINE_URL = "/backend/api/focus/";
+export const RESET_MACHINE_URL = "/backend/api/reset/";
