cdd7b5b48e0ceee11ee7cf54319f835d8920eddd
chirag4601 <chirag4601@gmail.com>
2025-01-22 01:26:25 +0530

    added cors for frontend

--- machine_project/app.js
+++ machine_project/app.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const sequelize = require("./config/database");
 const machineRoutes = require("./routes/machineRoutes");
 
 const app = express();
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(machineRoutes);
 
